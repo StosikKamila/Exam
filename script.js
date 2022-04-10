4d533293a5d41ce70ca8f6d61dec8d18b2a171e0
StosikKamila <stosik.kamila@gmail.com>
2022-04-10 20:24:36 +0200

    js exam

---
+++ script.js
@@ -0,0 +1,11 @@
+
+const plane = document.createElement("div");
+
+plane.innerText = "Kamila Stosik"
+plane.style.cssText = 
+`color: #070914;
+border:2px solid #7562E0;
+height:4h;
+width:10vw;
+position:relative;`
+document.body.append(plane);
